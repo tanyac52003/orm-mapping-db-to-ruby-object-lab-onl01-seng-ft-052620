d9fa96600a1dd4f52582ca67fc92eb18a8a7004d
Tanya Hodges <tanyac52003@gmail.com>
2020-06-25 13:32:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,9 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
     # create a new Student object given a row from the database
   end
 
