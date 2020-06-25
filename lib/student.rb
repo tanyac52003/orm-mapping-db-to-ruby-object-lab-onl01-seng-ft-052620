69e8a55a68d8ce0c3a848ad91532bcd0f283acdf
Tanya Hodges <tanyac52003@gmail.com>
2020-06-25 13:33:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,8 @@ class Student
     new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
     # create a new Student object given a row from the database
   end
 
